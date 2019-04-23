429854fb3c07c1b3f1347a197c5358c68c3737a9
unicorn-84 <unicorn-84@ya.ru>
2019-04-23 11:47:52 +0300

    Update assets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,5 @@
 
-window._ = require('lodash');
+// window._ = require('lodash');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,8 +8,8 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+    // window.Popper = require('popper.js').default;
+    // window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
 } catch (e) {}
