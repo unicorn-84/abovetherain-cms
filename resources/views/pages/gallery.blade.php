117fc1b21d540c0efc8c9f009923d26e448ee052
unicorn-84 <unicorn-84@ya.ru>
2019-05-02 00:47:10 +0300

    Update views

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -1,10 +1,4 @@
 @component('layouts.app', ['pageSelector' => $pageSelector])
-
-  {{--Styles--}}
-  @slot('styles')
-    <style>html.fon-loaded::before {background-image: url({{ mix('images/fon/mobile-common-fon.jpg') }});}@media (min-width: 992px) {html.fon-loaded::before {background-image: url({{ mix('images/fon/common-fon.jpg') }});}}</style>
-  @endslot
-
   {{--Header--}}
   @component('inc.header')
   @endcomponent
