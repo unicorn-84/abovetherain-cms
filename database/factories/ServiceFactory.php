c1ab43252d504e66061b7487d89f0479a8f8bf05
unicorn-84 <unicorn-84@ya.ru>
2019-05-12 23:09:52 +0300

    Update service

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -6,14 +6,17 @@ use App\Service;
 use Faker\Generator as Faker;
 
 $factory->define(Service::class, function (Faker $faker) {
+  $image = $faker->imageUrl(800, 533, false, true);
+  $title = substr($faker->sentence(2), 0, -1);
+  $content = '<img class="w-100 mb-4" src="' . $image . '"/>'. '<h1 class="text-center mb-3">' . $title . '</h1>' . '<p class="mb-0">' . $faker->realText($maxNbChars = 1000, $indexSize = 2) . '</p>';
   return [
-    'title' => substr($faker->sentence(2), 0, -1),
+    'title' => $title,
     'slug' => $faker->slug,
     'order' => $faker->randomDigitNotNull,
     'seo_title' => $faker->sentence(10),
     'description' => $faker->text($maxNbChars = 200),
     'seo_description' => $faker->text($maxNbChars = 20),
-    'poster' => $faker->imageUrl(800, 533, false, true),
-    'content' => $faker->realText($maxNbChars = 1000, $indexSize = 2),
+    'poster' => $image,
+    'content' => $content,
   ];
 });
