90ceb7c693df4b82e4e1279872ac9771eac956ba
unicorn-84 <unicorn-84@ya.ru>
2019-06-02 22:27:36 +0300

    Update project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,37 +74,37 @@
 <!--Body-->
 <body class="d-flex flex-column">
 <!--Yandex.Metrika counter-->
-<script>
-  (function (d, w, c) {
-    (w[c] = w[c] || []).push(function () {
-      try {
-        w.yaCounter51313063 = new Ya.Metrika2({
-          id: 51313063,
-          clickmap: true,
-          trackLinks: true,
-          accurateTrackBounce: true,
-          webvisor: true
-        });
-      } catch (e) {
-      }
-    });
+{{--<script>--}}
+  {{--(function (d, w, c) {--}}
+    {{--(w[c] = w[c] || []).push(function () {--}}
+      {{--try {--}}
+        {{--w.yaCounter51313063 = new Ya.Metrika2({--}}
+          {{--id: 51313063,--}}
+          {{--clickmap: true,--}}
+          {{--trackLinks: true,--}}
+          {{--accurateTrackBounce: true,--}}
+          {{--webvisor: true--}}
+        {{--});--}}
+      {{--} catch (e) {--}}
+      {{--}--}}
+    {{--});--}}
 
-    var n = d.getElementsByTagName('script')[0],
-      s = d.createElement('script'),
-      f = function () {
-        n.parentNode.insertBefore(s, n);
-      };
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = 'https://mc.yandex.ru/metrika/tag.js';
+    {{--var n = d.getElementsByTagName('script')[0],--}}
+      {{--s = d.createElement('script'),--}}
+      {{--f = function () {--}}
+        {{--n.parentNode.insertBefore(s, n);--}}
+      {{--};--}}
+    {{--s.type = 'text/javascript';--}}
+    {{--s.async = true;--}}
+    {{--s.src = 'https://mc.yandex.ru/metrika/tag.js';--}}
 
-    if (w.opera == '[object Opera]') {
-      d.addEventListener('DOMContentLoaded', f, false);
-    } else {
-      f();
-    }
-  })(document, window, 'yandex_metrika_callbacks2');
-</script>
+    {{--if (w.opera == '[object Opera]') {--}}
+      {{--d.addEventListener('DOMContentLoaded', f, false);--}}
+    {{--} else {--}}
+      {{--f();--}}
+    {{--}--}}
+  {{--})(document, window, 'yandex_metrika_callbacks2');--}}
+{{--</script>--}}
 <noscript>
   <div>
     <img src="https://mc.yandex.ru/watch/51313063" style="position:absolute; left:-9999px;" alt="">
