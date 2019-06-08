4ec7ce9df6ef9d763a27615301efa0965bbe2a7c
unicorn-84 <unicorn-84@ya.ru>
2019-06-08 18:09:51 +0300

    Изменены стили

--- test.js
+++ test.js
@@ -1,120 +1,260 @@
+bootW25: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'w-25',
+},
+bootW50: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'w-50',
+},
+bootW75: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'w-75',
+},
+bootW100: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'w-100',
+},
+bootWAuto: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'w-auto',
+},
+bootM0: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-0',
+},
+bootM1: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-1',
+},
+bootM2: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-2',
+},
+bootM3: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-3',
+},
+bootM4: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-4',
+},
+bootM5: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'm-5',
+},
+bootMT0: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-0',
+},
+bootMT1: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-1',
+},
+bootMT2: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-2',
+},
+bootMT3: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-3',
+},
+bootMT4: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-4',
+},
+bootMT5: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mt-5',
+},
+bootMR0: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-0',
+},
+bootMR1: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-1',
+},
+bootMR2: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-2',
+},
+bootMR3: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-3',
+},
+bootMR4: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-4',
+},
+bootMR5: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mr-5',
+},
+bootMB0: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-0',
+},
+bootMB1: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-1',
+},
+bootMB2: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-2',
+},
+bootMB3: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-3',
+},
+bootMB4: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-4',
+},
+bootMB5: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'mb-5',
+},
+bootML0: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-0',
+},
+bootML1: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-1',
+},
+bootML2: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-2',
+},
+bootML3: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-3',
+},
+bootML4: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-4',
+},
+bootML5: {
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
+    classes: 'ml-5',
+},
 bootP0: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-0',
 },
 bootP1: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-1',
 },
 bootP2: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-2',
 },
 bootP3: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-3',
 },
 bootP4: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-4',
 },
 bootP5: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'p-5',
 },
 bootPT0: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-0',
 },
 bootPT1: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-1',
 },
 bootPT2: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-2',
 },
 bootPT3: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-3',
 },
 bootPT4: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-4',
 },
 bootPT5: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pt-5',
 },
 bootPR0: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-0',
 },
 bootPR1: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-1',
 },
 bootPR2: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-2',
 },
 bootPR3: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-3',
 },
 bootPR4: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-4',
 },
 bootPR5: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pr-5',
 },
 bootPB0: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-0',
 },
 bootPB1: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-1',
 },
 bootPB2: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-2',
 },
 bootPB3: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-3',
 },
 bootPB4: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-4',
 },
 bootPB5: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pb-5',
 },
 bootPL0: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-0',
 },
 bootPL1: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-1',
 },
 bootPL2: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-2',
 },
 bootPL3: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-3',
 },
 bootPL4: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-4',
 },
 bootPL5: {
-  selector: 'img,p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button',
+  selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,span,a,input,label,button,img',
     classes: 'pl-5',
 },
