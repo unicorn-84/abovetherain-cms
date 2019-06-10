dfe2036bb72e66574c4f374525d76ee7c3e17a03
unicorn-84 <unicorn-84@ya.ru>
2019-06-10 13:31:50 +0300

    Update dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.21 on 2019-06-08 09:05:48.
+ * Generated for Laravel 5.8.21 on 2019-06-10 10:31:22.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
